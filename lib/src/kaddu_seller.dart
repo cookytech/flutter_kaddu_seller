9261f5efb64a675892638e77e4e48ad71c2e54c3
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-03 08:55:37 +0530

    adds google services stuff

--- lib/src/kaddu_seller.dart
+++ lib/src/kaddu_seller.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kaddu_seller/src/items_listing_screen.dart';
 
 class KadduSeller extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ItemsListingScreen(),
+    );
   }
 }
